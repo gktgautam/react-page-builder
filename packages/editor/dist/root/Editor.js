6a5ff354ac5786843c01311c505eefc1155684d0
Gautam Kumar <gktgautam@gmail.com>
2025-08-15 13:00:38 +0530

    move web to root

--- packages/editor/dist/root/Editor.js
+++ packages/editor/dist/root/Editor.js
@@ -3,6 +3,7 @@ import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { Sidebar } from "../components/Sidebar";
 import { EditorCanvas } from "../components/EditorCanvas";
 import { LayersPanel } from "../components/LayersPanel";
+import { PropertyPanel } from "../components/PropertyPanel";
 export function Editor() {
-    return (_jsxs("div", { className: "grid grid-cols-[16rem_1fr_16rem] h-screen", children: [_jsx(Sidebar, {}), _jsx(EditorCanvas, {}), _jsx(LayersPanel, {})] }));
+    return (_jsxs("div", { className: "grid grid-cols-[16rem_1fr_16rem_16rem] h-screen", children: [_jsx(Sidebar, {}), _jsx(EditorCanvas, {}), _jsx(LayersPanel, {}), _jsx(PropertyPanel, {})] }));
 }
